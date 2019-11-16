ef3c82221950014b80cf86554e59c14907fe34b9
Abda Shaffan Diva <abdashaffan@gmail.com>
2019-11-16 16:19:29 +0700

    Debug 1st and 4th feature implementation

--- src/org/K03G04Tubes2/NasabahService.java
+++ src/org/K03G04Tubes2/NasabahService.java
@@ -3,16 +3,14 @@ package org.K03G04Tubes2;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
-import java.sql.SQLException;
 
 @WebService
 public interface NasabahService {
 
     @WebMethod
-    boolean isValidNasabahNumber(@WebParam(name = "account_number") int accNum) throws SQLException; //layanan1
+    boolean isValidNasabahNumber(@WebParam(name = "account_num") int accNum); //layanan1
 
     @WebMethod
-    int createNasabahVirtualAccount(@WebParam(name = "account_number") int accNum) throws SQLException; //layanan4
-
+    int createNasabahVirtualAccount(@WebParam(name = "account_num") int accNum); //layanan4
 
 }
--- src/org/K03G04Tubes2/repository/WSBankDB.java
+++ src/org/K03G04Tubes2/repository/WSBankDB.java
@@ -20,46 +20,61 @@ public class WSBankDB {
         }
     }
 
-    public boolean isValidAccountNum(int accNum) throws SQLException {
-        String query = "SELECT * FROM nasabah WHERE account_num = ?";
-        stmt = connection.prepareStatement(query);
-        stmt.setInt(1, accNum);
-        ResultSet result = stmt.executeQuery();
-        return result.next();
+    public boolean isValidAccountNum(int accNum){
+        try {
+            String query = "SELECT * FROM nasabah WHERE account_num = ?";
+            stmt = connection.prepareStatement(query);
+            stmt.setInt(1, accNum);
+            ResultSet result = stmt.executeQuery();
+            return result.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
-    public int createVirtualAccountNumber(int accNum) throws SQLException {
+    public int createVirtualAccountNumber(int accNum){
         //prekondisi: accNum UDAH VALID (udah ada di db sebelumnya)
-        int virtualAccNum = generateVirtualAccNum();
-        String query = "INSERT INTO virtual_account (virtual_account_num, account_num) VALUES (?, ?)";
-        stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-        stmt.setInt(1, virtualAccNum);
-        stmt.setInt(2, accNum);
-        int executeStatus = stmt.executeUpdate();
-        if (executeStatus == 0 || executeStatus == -1) {
-            throw new SQLException("Failed to create new virtual account for account number" + accNum);
-        }
-        try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
-            if (generatedKeys.next()) {
+        try {
+            int virtualAccNum = generateVirtualAccNum();
+            String query = "INSERT INTO virtual_account (virtual_account_num, account_num) VALUES (?, ?)";
+            stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            stmt.setInt(1, virtualAccNum);
+            stmt.setInt(2, accNum);
+            int executeStatus = stmt.executeUpdate();
+            if (executeStatus == 0 || executeStatus == -1) {
+                throw new SQLException("Failed to create new virtual account for account number" + accNum);
+            }
+            ResultSet generatedKeys = stmt.getGeneratedKeys();
+                if (generatedKeys.next()) {
 
-                int id = generatedKeys.getInt(1);
-                return getVirtualAccountById(id);
-            }
-            else {
-                throw new SQLException("Creating user failed, no ID obtained.");
-            }
+                    int id = generatedKeys.getInt(1);
+                    return getVirtualAccountById(id);
+                }
+                else {
+                    throw new SQLException("Creating user failed, no ID obtained.");
+                }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -999;
         }
     }
 
-    private int getVirtualAccountById(int id) throws  SQLException {
-        String query = "SELECT virtual_account_num FROM virtual_account WHERE id = ?";
-        stmt = connection.prepareStatement(query);
-        stmt.setInt(1, id);
-        ResultSet result = stmt.executeQuery();
-        if (result.next()){
-            return result.getInt("virtual_account_num");
+    private int getVirtualAccountById(int id)  {
+        try {
+            String query = "SELECT virtual_account_num FROM virtual_account WHERE id = ?";
+            stmt = connection.prepareStatement(query);
+            stmt.setInt(1, id);
+            ResultSet result = stmt.executeQuery();
+            if (result.next()){
+                return result.getInt("virtual_account_num");
+            }
+            return -1;
         }
-        return -1;
+        catch (SQLException e) {
+            e.printStackTrace();
+            return -999;
+        }
     }
 
     private int generateVirtualAccNum() {
