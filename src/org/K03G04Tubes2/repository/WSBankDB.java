8f2d213125582fcb4f9c84caf8ead55247fb8e49
Alam <13517096@std.stei.itb.ac.id>
2019-11-19 08:49:09 +0700

    changed password and username of db

--- src/org/K03G04Tubes2/repository/WSBankDB.java
+++ src/org/K03G04Tubes2/repository/WSBankDB.java
@@ -1,12 +1,14 @@
 package org.K03G04Tubes2.repository;
 
+import org.K03G04Tubes2.model.Nasabah;
+
 import java.sql.*;
 
 public class WSBankDB {
 //    private static final String PUBLIC_EC2_DNS = "ec2-52-23-241-205.compute-1.amazonaws.com";
     private static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/ws_bank?serverTimezone=UTC";
-    private static final String DB_USER = "abda";
-    private static final String DB_PASS = "abda";
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = "root";
     private static Connection connection;
     private static PreparedStatement stmt;
 
@@ -19,6 +21,36 @@ public class WSBankDB {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public Nasabah getNasabah(int accNum){
+        try{
+            String query = "SELECT * FROM nasabah WHERE account_num = ?";
+            stmt = connection.prepareStatement(query);
+            stmt.setInt(1,accNum);
+            ResultSet result = stmt.executeQuery();
+
+            // Create and return new nasabah
+            if(result.next()==false){
+                //empty nasabah
+                //use dummy accountNum -999 instead of returning null
+                Nasabah n = new Nasabah("",-999);
+                return n;
+            } else {
+                Nasabah n = new Nasabah(result.getString("name"),accNum);
+                n.setBalance(result.getInt("balance"));
+                System.out.println(n.getName());
+                return n;
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+            return null;
+        } catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
+
     }
 
     public boolean isValidAccountNum(int accNum){
